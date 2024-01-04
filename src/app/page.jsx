44cbc601ba5adf4ad92add7336efb482c043fd64
KaifNazeer <muhammadkaifnazeer99@gmail.com>
2024-01-04 10:09:46 +0500

    css responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,17 @@ import styles from "./homepage.module.css";
 
 export default function Home() {
   return <div>
-    <Link href="/">Hello</Link>
+    <Link href="/">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, error quisquam minima iusto ad ex quam et sequi dolorem deserunt magni tenetur unde quaerat officia rerum consequatur neque totam corrupti?
+      </h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
+    </Link>
   </div>;
 }
