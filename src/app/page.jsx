e34dff8c596e858f70f95710f9e8b6bf3966e41e
KaifNazeer <muhammadkaifnazeer99@gmail.com>
2024-01-04 10:26:11 +0500

    created navbar and footer component and added write css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,18 +2,9 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>
-    <Link href="/">
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, error quisquam minima iusto ad ex quam et sequi dolorem deserunt magni tenetur unde quaerat officia rerum consequatur neque totam corrupti?
-      </h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam praesentium adipisci tenetur, labore hic velit eum voluptates ut reprehenderit tempora consequuntur eligendi corporis sed vel nemo perspiciatis, pariatur ratione illo.
-    </Link>
-  </div>;
+  return (
+    <div>
+      
+    </div>
+  )
 }
