c6457d28a30e994cdd799d92dcb127c509e94e36
KaifNazeer <muhammadkaifnazeer99@gmail.com>
2024-01-17 16:56:50 +0500

    updated authLinks

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -40,7 +40,7 @@ const AuthLinks = () => {
                     ) : (
                         <>
                             <Link href="/write">Write</Link>
-                            <span className={styles.link}>Logout</span>
+                            <span className={styles.link} onClick={signOut}>Logout</span>
                         </>
                     )}
                 </div>
