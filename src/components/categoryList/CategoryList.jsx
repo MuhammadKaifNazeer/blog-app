9634aea13f1d99c23a0b322c3428025b1a440ee1
KaifNazeer <muhammadkaifnazeer99@gmail.com>
2024-01-18 22:05:19 +0500

    solved problems in CatagoryList.jsx

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -17,34 +19,44 @@ const getData = async () => {
   } catch (error) {
     console.error("Error fetching data:", error);
     // Handle the error appropriately, e.g., display an error message to the user
+    return [];
   }
 };
 
+const CategoryList = () => {
+  const [data, setData] = useState([]);
 
-const CategoryList = async () => {
-  const data = await getData();
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getData();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {data?.map((item) => (
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles[item.slug]}`}
-            key={item._id}
-          >
-            {item.img && (
-              <Image
-                src={item.img}
-                alt=""
-                width={32}
-                height={32}
-                className={styles.image}
-              />
-            )}
-            {item.title}
-          </Link>
-        ))}
+        {data.map((item) => (
+            <Link
+              href={`/blog?cat=${item.slug}`}
+              className={`${styles.category} ${styles[item.slug]}`}
+              key={item._id}
+            >
+              {item.img && (
+                <Image
+                  src={item.img}
+                  alt=""
+                  width={32}
+                  height={32}
+                  className={styles.image}
+                />
+              )}
+              {item.title}
+            </Link>
+          ))}
       </div>
     </div>
   );
